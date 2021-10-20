7dddfe23eee6436a60f82667c8ca975c349cfa52
Felipe Valtl de Mello <3065339+valtlfelipe@users.noreply.github.com>
2021-10-20 19:08:50 -0300

    feat: add page selector in sidebar menu (#24)

--- lib/components/sidebar_menu.dart
+++ lib/components/sidebar_menu.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:statuscenter/dialogs/select_page.dart';
 import 'package:statuscenter/models/auth_data.dart';
+import 'package:statuscenter/services/auth_service.dart';
 import 'package:statuscenter/utils/color.dart';
+import 'package:statuscenter/models/page.dart' as PageModel;
 
 class SidebarMenu extends StatelessWidget {
   final AuthData authData;
@@ -16,78 +19,108 @@ class SidebarMenu extends StatelessWidget {
     }
   }
 
+  _changedPage(context, PageModel.Page page) async {
+    await AuthService.setPage(page);
+    return Navigator.pushReplacementNamed(context, '/home');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Container(
-        padding: EdgeInsets.zero,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: ACCENT_COLOR,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: ACCENT_COLOR,
+            ),
+            padding: EdgeInsets.zero,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(16),
+                  child: Text(
                     'Status Center',
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 24,
                     ),
                   ),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text(
-                          this.authData != null ? this.authData.page.name : '',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                          ),
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      ListTile(
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return SelectPageDialog(
+                                currentPage: this.authData.page,
+                                onChange: (PageModel.Page page) {
+                                  this._changedPage(context, page);
+                                },
+                              );
+                            },
+                          );
+                        },
+                        title: this.authData != null
+                            ? Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    this.authData.page.name,
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                  Text(
+                                    'ID: ${this.authData.page.id}',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                ],
+                              )
+                            : Text('Select page'),
+                        trailing: Icon(
+                          Icons.chevron_right,
+                          color: Colors.white,
                         ),
-                        Text(
-                          this.authData != null
-                              ? 'ID: ${this.authData.page.id}'
-                              : '',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 12,
-                          ),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
             ),
-            ListTile(
-              leading: Icon(Icons.dashboard),
-              title: Text('Dashboard'),
-              selected: this.currentPage == '/home' ? true : false,
-              onTap: () => _handleTap(context, '/home'),
-            ),
-            ListTile(
-              leading: Icon(Icons.announcement),
-              title: Text('Incidents'),
-              selected: this.currentPage == '/incidents' ? true : false,
-              onTap: () => _handleTap(context, '/incidents'),
-            ),
-            Divider(),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.pushNamed(context, '/settings');
-              },
-            ),
-          ],
-        ),
+          ),
+          ListTile(
+            leading: Icon(Icons.dashboard),
+            title: Text('Dashboard'),
+            selected: this.currentPage == '/home' ? true : false,
+            onTap: () => _handleTap(context, '/home'),
+          ),
+          ListTile(
+            leading: Icon(Icons.announcement),
+            title: Text('Incidents'),
+            selected: this.currentPage == '/incidents' ? true : false,
+            onTap: () => _handleTap(context, '/incidents'),
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings'),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.pushNamed(context, '/settings');
+            },
+          ),
+        ],
       ),
     );
   }
---
+++ lib/dialogs/select_page.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:statuscenter/clients/pages_client.dart';
+import 'package:statuscenter/components/loading.dart';
+import 'package:statuscenter/models/page.dart' as PageModel;
+
+class SelectPageDialog extends StatefulWidget {
+  final PageModel.Page currentPage;
+  final List<PageModel.Page> pages;
+  final void Function(PageModel.Page) onChange;
+
+  const SelectPageDialog({Key key, this.currentPage, this.pages, this.onChange})
+      : super(key: key);
+
+  @override
+  _SelectPageDialogState createState() => _SelectPageDialogState();
+}
+
+class _SelectPageDialogState extends State<SelectPageDialog> {
+  bool _isLoading = false;
+  List<PageModel.Page> _pages;
+
+  @override
+  void initState() {
+    if (widget.pages == null) {
+      _isLoading = true;
+      _loadData();
+    } else {
+      _pages = widget.pages;
+    }
+    super.initState();
+  }
+
+  Future _loadData() async {
+    List<PageModel.Page> pages = await new PagesClient().getPages();
+    setState(() {
+      _pages = pages;
+      _isLoading = false;
+    });
+  }
+
+  void _handleRadioValueChange(PageModel.Page page) {
+    Navigator.of(context).pop();
+    widget.onChange(page);
+  }
+
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: _isLoading
+              ? [
+                  Container(padding: EdgeInsets.all(10), child: Loading()),
+                ]
+              : [
+                  Container(
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      'Your pages:',
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                  ),
+                  ..._pages.map((PageModel.Page page) {
+                    return InkWell(
+                      onTap: () => _handleRadioValueChange(page),
+                      child: Container(
+                        padding: EdgeInsets.only(left: 10, right: 10),
+                        child: Row(
+                          children: [
+                            Radio(
+                              onChanged: (value) =>
+                                  _handleRadioValueChange(value),
+                              groupValue: widget.currentPage != null ??
+                                  widget.currentPage.id,
+                              value: page.id,
+                            ),
+                            Text(page.name),
+                          ],
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ],
+        ),
+      ),
+    );
+  }
+}
